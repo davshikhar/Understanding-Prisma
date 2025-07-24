c82f6190644ce23819322b51f011218a73ff489a
davshikhar <shikharyadav900@gmail.com>
2025-07-24 12:30:33 +0530

    Updated seed.ts

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -4,3 +4,31 @@ const client = new PrismaClient();
 
 //this file is to seed some data into the db to show some data to the user 
 // when they maybe clone the repo
+
+//this file adds some dummy data to the db for user's convenience
+// so that when new user clones this codebase they see some data in db
+async function createDummyUser() {
+    try{
+    await client.user.create({
+        data: {
+            username: "xyz",
+            age: 10,
+            city: "abc",
+            password: "def123",
+            todo: {
+                create: {
+                    description: "Go gym",
+                    title: "Gym",
+                    done: false
+                }
+            }
+        }
+    });
+    }
+    catch(e){
+        console.log("Error sending the data: "+e);
+        throw e;
+    }
+}
+
+createDummyUser();
